5c9ae00e2d1075f9296cebc943cbc5219e2b42c8
Bryan Schneller <bryan.schneller@gmail.com>
2025-03-24 12:54:59 -0400

    Add user model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, through: :posts, dependent: :destroy
+  has_many :friend_requests, foreign_key: :sender_id, dependent: :destroy
+  has_many :received_friend_requests, class_name: "FriendRequest", foreign_key: :receiver_id, dependent: :destroy
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
